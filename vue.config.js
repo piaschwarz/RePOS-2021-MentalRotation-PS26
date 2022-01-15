7c480dddf2e8d9d96bd11d2e0947e7a05d77f2c5
piaschwarz <38730179+piaschwarz@users.noreply.github.com>
2022-01-15 16:48:40 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   configureWebpack: {
     
-    if (process.env.NODE_ENV === 'production') {
-        config.mode = 'production';
-        config.performance = {
-          maxEntrypointSize: 10000000,
-          maxAssetSize: 30000000
-        }
-    }    
+    config.performance = {
+      maxEntrypointSize: 10000000,
+      maxAssetSize: 30000000
+    }
     
     module: {
       rules: [
